9b878579ded0fdbe5f5b524970163c7c700dc1a3
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2026-05-13 22:51:50 +0200

    expose them

--- polars_stats/__init__.py
+++ polars_stats/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from polars_stats._internal import __version__ as __version__
-from polars_stats.distributions import Bernoulli
+from polars_stats.distributions._base import ContinuousDistribution, DiscreteDistribution
+from polars_stats.distributions._bernoulli import Bernoulli
 
-__all__ = ("Bernoulli", "__version__")
+__all__ = ("Bernoulli", "ContinuousDistribution", "DiscreteDistribution", "__version__")
--- polars_stats/distributions/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from polars_stats.distributions._base import ContinuousDistribution, DiscreteDistribution
-from polars_stats.distributions._bernoulli import Bernoulli
-
-__all__ = ("Bernoulli", "ContinuousDistribution", "DiscreteDistribution")
